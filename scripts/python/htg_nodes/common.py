48ceb0642c8974b1eed00c68066f41aec5803196
Baldrax <thatguybaldrax@gmail.com>
2022-02-26 21:03:05 -0800

    Fixed offset issue with some proxies Fixed orientation of Cliff Proxy Added Tree 01 Proxy Moved Asset Proxies out of the Terrain node and in to their own .hip file Added new OBJ_TaleSpire_Shared_Data HDA Added new SharedData class to hgt_nodes.common Nodes no longer try to reference the OBJ_TaleSpire_Terrain node for shared data, they now reference the shared node which should be more efficient and predictable.

--- scripts/python/htg_nodes/common.py
+++ scripts/python/htg_nodes/common.py
@@ -6,3 +6,28 @@ def ts_assets_menu():
     """Get a menu list of specific TaleSpire Asset IDs."""
     # TODO: For implementation
     pass
+
+
+# Shared Data Node
+def get_ts_database_node():
+    shared_data_node = SharedData()
+    return shared_data_node.get_database_node()
+
+
+class SharedData:
+
+    def __init__(self):
+        """This class accesses and creates the TaleSpire_Shared_Data node containing the TS database
+        needed from various nodes. This keeps the data in one place where it is cached for efficiency."""
+        self.node_name = '_TaleSpire_Shared_Data_'
+        self.shared_data_node = hou.node('/obj/{}'.format(self.node_name))
+        if not self.shared_data_node:
+            self.shared_data_node = hou.node('/obj').createNode('TaleSpire_Shared_Data', node_name=self.node_name)
+            self.hide()
+
+    def hide(self, state=True):
+        self.shared_data_node.hide(state)
+
+    def get_database_node(self):
+        database_node = hou.node('{}/data/TS_Database'.format(self.shared_data_node.path()))
+        return database_node
