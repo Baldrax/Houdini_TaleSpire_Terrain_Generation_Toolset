9ac1500023516a16bf2b4136716e5d2f98cae62b
Baldrax <thatguybaldrax@gmail.com>
2022-03-07 00:17:04 -0800

    Improved ground contact behaviour. TaleSpire Object Nodes now have options to better control how the objects sit on the ground. Added button on TaleSpire Biome node to refresh the biome, needed when adding new objects to a biome.

--- scripts/python/htg/nodes/OBJ_TaleSpire_Biome.py
+++ scripts/python/htg/nodes/OBJ_TaleSpire_Biome.py
@@ -2,6 +2,14 @@ import hou
 import htg.utils
 
 
+def refresh_biome(node=None):
+    cook_paths = ['/biome_pts/biome_cat_points']
+
+    for cook_path in cook_paths:
+        cook_node = hou.node(node.path()+cook_path)
+        cook_node.cook(force=True)
+
+
 def edit_objects(node=None):
     dest_node = hou.node(node.path() + '/biome_objects')
     htg.utils.set_network(node, dest_node)
