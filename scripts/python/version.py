092e54106213d701b8875ea736606b3d267d8990
Baldrax <thatguybaldrax@gmail.com>
2024-07-14 15:32:35 -0700

    Updated Version

--- scripts/python/version.py
+++ scripts/python/version.py
@@ -1,4 +1,4 @@
 # This is the version of the release.
 # Should be updated whenever a push is done.
 # Include branch name on daily pushes. Only upgrade Major and Minor version when Pulling into the master branch.
-version = '0.20.5-dev'
+version = '0.20.6-dev'
