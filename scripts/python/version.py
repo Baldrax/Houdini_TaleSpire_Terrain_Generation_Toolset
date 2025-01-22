b263c597d8d4aa92df1d4c36b343e8ef446e14b1
Baldrax <thatguybaldrax@gmail.com>
2025-01-21 17:29:34 -0800

    Updated version to v0.23.2-dev

--- scripts/python/version.py
+++ scripts/python/version.py
@@ -1,4 +1,4 @@
 # This is the version of the release.
 # Should be updated whenever a push is done.
 # Include branch name on daily pushes. Only upgrade Major and Minor version when Pulling into the master branch.
-version = '0.23.1-dev'
+version = '0.23.2-dev'
