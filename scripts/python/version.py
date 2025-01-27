03e6c3353bbca298d5c92861cf430a7429cdded9
Baldrax <100365731+Baldrax@users.noreply.github.com>
2025-01-26 20:55:32 -0800

    Update version

--- scripts/python/version.py
+++ scripts/python/version.py
@@ -2,4 +2,4 @@
 # Should be updated whenever a push is done.
 # Include branch name on daily pushes. Only upgrade Major and Minor version when Pulling into the master branch.
 
-version = '0.23.4-dev'
+version = '0.24.0'
