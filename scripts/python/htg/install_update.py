38c7c2a339ef71278df0effa3748c064c7203b4a
Baldrax <thatguybaldrax@gmail.com>
2025-02-01 22:10:58 -0800

    WIP commit

--- scripts/python/htg/install_update.py
+++ scripts/python/htg/install_update.py
@@ -2,12 +2,14 @@ import json
 import os
 import sys
 
+from distutils.version import LooseVersion
 from pathlib import Path
 
 import hou
 
 from PySide2.QtWidgets import (
-    QPushButton, QDialog, QStackedWidget, QVBoxLayout, QLabel, QLineEdit, QHBoxLayout, QCheckBox
+    QPushButton, QDialog, QStackedWidget, QVBoxLayout, QLabel, QLineEdit, QHBoxLayout, QCheckBox,
+    QComboBox, QSizePolicy, QTextEdit
 )
 from PySide2.QtCore import Qt
 
@@ -24,6 +26,25 @@ def make_package_file(htg_dir=None):
         json.dump(json_data, f, indent=4)
 
 
+def get_branches():
+    import requests
+    if hasattr(hou.session, "htg_branches") and hou.session.htg_branches is not None:
+        return hou.session.htg_branches
+    else:
+        repo_url = os.environ.get("HTG_REPO_URL")
+        if repo_url:
+            branches_url = f"{repo_url}/branches"
+
+            response = requests.get(branches_url)
+            if response.status_code == 200:
+                branches = response.json()
+                branch_list = []
+                for branch in branches:
+                    branch_list.append(branch["name"])
+                hou.session.htg_branches = branch_list
+                return branch_list
+
+
 def get_releases():
     import requests
     if hasattr(hou.session, "htg_releases") and hou.session.htg_releases is not None:
@@ -38,13 +59,39 @@ def get_releases():
             return releases
 
 
+def get_current_version():
+    from version import version as current_version
+    return current_version
+
+
+def version_text(branch="dev", new_version="00.00.00"):
+    current_version = get_current_version()
+    is_newer = LooseVersion(new_version.lstrip("v")) > LooseVersion(current_version)
+    if is_newer:
+        ver_color = "green"
+    else:
+        ver_color = "red"
+
+    text = f"""<p>Current Version: <span style="color:yellow; font-weight:bold;">v{current_version}</span></p>"""
+
+    if branch == "release":
+        text +=    f"""<p>Release Version: <span style="color:{ver_color}; font-weight:bold;">{new_version}</span></p>"""
+    else:
+        text += f"""<p>Branch Version: <span style="color:red; font-weight:bold;">Unknown</span>-<span 
+        style="font-weight:bold;">{branch}</span></p>"""
+        text += f"""<p></p><p><span style="color:red; font-weight:bold;">Warning!</span> 
+        Branch versions could be older than your current version.</p>"""
+    return text
+
 class InstallDialog(QDialog):
+
+    LABEL_WIDTH = 50
 
     def __init__(self, cmd_path=None):
         parent = hou.qt.mainWindow()
         super().__init__(parent, Qt.Window)
         self.setWindowTitle("HTG Toolset Install/Update")
-        self.setMinimumWidth(400)
+        self.setMinimumWidth(600)
 
         hou.session.install_dialog = self
 
@@ -78,6 +125,11 @@ class InstallDialog(QDialog):
         self.install_in_place = None
         self.install_location = None
         self.browse_button = None
+        self.branch_combo = None
+        self.release_combo = None
+        self.version_label = None
+        self.install_window = None
+        self.done_button = None
 
         self.pages = []
         self.stack = QStackedWidget(self)
@@ -93,8 +145,8 @@ class InstallDialog(QDialog):
         self.pages.append("Update")
         self.init_update_page()
 
-        self.pages.append("CopyingFiles")
-        self.init_progress_page()
+        self.pages.append("Installation")
+        self.init_installation_page()
 
         self.stack.setCurrentIndex(self.pages.index(self.start_page))
 
@@ -149,7 +201,7 @@ class InstallDialog(QDialog):
             self.install_location.setText(directory)
 
     def start_installation(self):
-        pass
+        self.stack.setCurrentIndex(self.pages.index("Installation"))
 
     def init_manual_update_page(self):
         page_setup = QVBoxLayout()
@@ -169,16 +221,98 @@ class InstallDialog(QDialog):
 
     def init_update_page(self):
         page_setup = QVBoxLayout()
-        label = QLabel("Update Page")
-        page_setup.addWidget(label)
+        page_setup.setAlignment(Qt.AlignTop)
+
+        branch_layout = QHBoxLayout()
+        branch_label = QLabel("Branch:")
+        branch_label.setFixedWidth(self.LABEL_WIDTH)
+        label_height = branch_label.sizeHint().height()
+        branch_label.setAlignment(Qt.AlignRight | Qt.AlignVCenter)
+
+        self.branch_combo = QComboBox()
+        self.branch_combo.addItem("Current Release")
+        self.branch_combo.addItem("Older Release")
+        skip_branches = ("master", "main")
+        skip_branches = ()
+        for branch in get_branches():
+            if branch not in skip_branches:
+                self.branch_combo.addItem(branch)
+        self.branch_combo.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+        self.branch_combo.setFixedHeight(label_height+10)
+
+        self.release_combo = QComboBox()
+        for release in get_releases():
+            self.release_combo.addItem(release["name"])
+        self.release_combo.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+        self.release_combo.setFixedHeight(label_height + 10)
+        self.release_combo.setVisible(False)
+
+        self.branch_combo.currentIndexChanged.connect(self.branch_updated)
+        self.release_combo.currentIndexChanged.connect(self.branch_updated)
+
+        branch_layout.addWidget(branch_label)
+        branch_layout.addWidget(self.branch_combo)
+        branch_layout.addWidget(self.release_combo)
+        branch_layout.setAlignment(Qt.AlignLeft | Qt.AlignVCenter)
+
+        page_setup.addLayout(branch_layout)
+
+        self.version_label = QLabel("")
+        self.version_label.setAlignment(Qt.AlignLeft)
+
+        page_setup.addWidget(self.version_label)
+
+        button_layout = QHBoxLayout()
+        button_layout.setAlignment(Qt.AlignRight | Qt.AlignBottom)
+        dl_cancel_button = QPushButton("Cancel")
+        dl_cancel_button.clicked.connect(self.reject)
+        dl_install_button = QPushButton("Download/Install")
+        dl_install_button.clicked.connect(self.start_installation)
+        button_layout.addWidget(dl_cancel_button)
+        button_layout.addWidget(dl_install_button)
+
+        page_setup.addStretch()
+        page_setup.addLayout(button_layout)
+
         widget = QDialog()
         widget.setLayout(page_setup)
         self.stack.addWidget(widget)
 
-    def init_progress_page(self):
+        self.branch_updated()
+
+    def branch_updated(self):
+        branch_value = self.branch_combo.currentText()
+        release_visible = branch_value == "Older Release"
+
+        self.release_combo.setVisible(release_visible)
+
+        branch = branch_value
+        if branch in ["Current Release", "Older Release"]:
+            branch = "release"
+
+        new_version = self.release_combo.currentText()
+        if branch_value == "Current Release":
+            new_version = get_releases()[0]["name"]
+        self.version_label.setText(version_text(branch=branch, new_version=new_version))
+
+    def init_installation_page(self):
         page_setup = QVBoxLayout()
-        label = QLabel("Progress Page")
+        page_setup.setAlignment(Qt.AlignTop)
+        label = QLabel("Installing...")
+        self.install_window = QTextEdit()
+        self.install_window.setReadOnly(True)
+
         page_setup.addWidget(label)
+        page_setup.addWidget(self.install_window)
+
+        button_layout = QHBoxLayout()
+        button_layout.setAlignment(Qt.AlignRight)
+        self.done_button = QPushButton("Done")
+        self.done_button.setEnabled(False)
+        button_layout.addWidget(self.done_button)
+
+        page_setup.addLayout(button_layout)
+
         widget = QDialog()
         widget.setLayout(page_setup)
         self.stack.addWidget(widget)
@@ -198,10 +332,13 @@ def show(cmd_path=None):
 # This checks to see if this script was executed from a menu within Houdini, if so it launches the dialog.
 try:
     open_dialog = "toolname" in kwargs
-    show()
 except NameError:
     open_dialog = False
 
+if open_dialog:
+    show()
+
+# This runs if executed from the Run command in Houdini or from the command line.
 if __name__ == "__main__":
     try:
         cmd_base_dir = sys.argv[1]
