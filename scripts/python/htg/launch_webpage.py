7b1115a230595a783c83e08a0c1fc043a8c15d8f
Baldrax <thatguybaldrax@gmail.com>
2026-02-02 18:41:52 -0800

    Updated Discord Link.

--- scripts/python/htg/launch_webpage.py
+++ scripts/python/htg/launch_webpage.py
@@ -2,11 +2,13 @@
 import sys
 import webbrowser
 
+dco = "Wx54CAtz4H"
+dc = "_uNGYt3XRfy_"
 
 urls = {
     "github": "https://github.com/Baldrax/Houdini_TaleSpire_Terrain_Generation_Toolset",
     "wiki": "https://github.com/Baldrax/Houdini_TaleSpire_Terrain_Generation_Toolset/wiki",
-    "discord": "https://discord.com/invite/Wx54CAtz4H"
+    "discord": f"https://discord.com/invite/{dc.strip('_')}"
 }
 
 webbrowser.open(urls[sys.argv[1]], new=0, autoraise=True)
